29e42b75ad9211b6f5324dab6c3bf44b175d1a03
Alov Maxim <alovmax@yandex.ru>
2016-04-18 08:31:13 +0300

    Added http parser sample

--- src/main/java/com/orcchg/zserver/server/WorkerRunnable.java
+++ src/main/java/com/orcchg/zserver/server/WorkerRunnable.java
@@ -1,6 +1,13 @@
 package com.orcchg.zserver.server;
 
 import com.orcchg.zserver.database.DatabaseHelper;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.entity.ContentLengthStrategy;
+import org.apache.http.impl.entity.StrictContentLengthStrategy;
+import org.apache.http.impl.io.*;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,9 +32,35 @@ public class WorkerRunnable implements Runnable {
             OutputStream output = mClientSocket.getOutputStream();
             String result = "";
             try {
+                HttpTransportMetricsImpl metrics = new HttpTransportMetricsImpl();
+                SessionInputBufferImpl buffer = new SessionInputBufferImpl(metrics, 2048);
+                buffer.bind(input);
+                DefaultHttpRequestParser parser = new DefaultHttpRequestParser(buffer);
+                HttpRequest request = parser.parse();
+
+                InputStream contentStream = null;
+                if (request instanceof HttpEntityEnclosingRequest) {
+                    ContentLengthStrategy contentLengthStrategy = StrictContentLengthStrategy.INSTANCE;
+                    long len = contentLengthStrategy.determineLength(request);
+                    if (len == ContentLengthStrategy.CHUNKED) {
+                        contentStream = new ChunkedInputStream(buffer);
+                    } else if (len == ContentLengthStrategy.IDENTITY) {
+                        contentStream = new IdentityInputStream(buffer);
+                    } else {
+                        contentStream = new ContentLengthInputStream(buffer, len);
+                    }
+                }
+
+                System.out.println("Request: " + request.getRequestLine().getUri());
+                if (contentStream != null) {
+                    String body = IOUtils.toString(contentStream, "UTF-8");
+                    System.out.println("Body: " + body);
+                }
                 result = mDbHelper.testQuery();
                 System.out.println("Result: " + result.length());
             } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (HttpException e) {
                 e.printStackTrace();
             }
             long time = System.currentTimeMillis();
---
+++ src/main/java/com/orcchg/zserver/tokenizer/RequestToken.java
@@ -0,0 +1,7 @@
+package com.orcchg.zserver.tokenizer;
+
+public class RequestToken {
+
+    private String mHttpMethod;
+    private String mServerMethod;
+}
